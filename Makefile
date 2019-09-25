97b31559a93b4f460e5756e1bada426790f552dd
Iskander Karibzhanov <ikarib@gmail.com>
2019-09-25 15:46:11 -0400

    test runs on K80, P100

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = nvcc
 NAME = estimate
 DFLS = DFLS
-CFLAGS = -g -I${DFLS}
+CFLAGS = -O2 -I${DFLS}
 LDFLAGS = -L${DFLS}
 
 default: ${NAME}
@@ -11,3 +11,12 @@ clean:
 
 ${NAME}: ${NAME}.cu
 	${CC} $(CFLAGS) ${LDFLAGS} $< -o $@ -lnewuoa_h
+
+test: ${NAME}.out ${NAME}-K80.out
+	@if diff $^ >${NAME}-test.diff; then \
+	    msg="test of C version successful"; \
+	    /bin/echo -e "\e[1;32m$${msg}\e[0;39;49m"; \
+	else \
+	    msg="test of C version failed (see file ${NAME}-test.diff)"; \
+	    /bin/echo -e "\e[1;31m$${msg}\e[0;39;49m"; \
+	fi
--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --account=def-iskander
+##SBATCH --account=def-iskander
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 ##SBATCH --gres=gpu:v100:8
 ##SBATCH --exclusive
 #SBATCH --cpus-per-task=1
@@ -11,5 +11,7 @@
 #SBATCH --job-name=HANK
 #SBATCH --error=estimate.err
 #SBATCH --output=estimate.out
-module load arch/avx512 StdEnv/2018.3
-valgrind --tool=memcheck --leak-check=yes --show-reachable=yes ./estimate 12
+#module load arch/avx512 StdEnv/2018.3
+#valgrind -v --main-stacksize=20787144 --max-stackframe=17787144 --gen-suppressions=yes --suppressions=./ignore --tool=memcheck --leak-check=yes --show-reachable=yes ./estimate 12
+ulimit -s unlimited
+./estimate 24
